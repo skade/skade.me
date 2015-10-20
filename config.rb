b9724c595ea1112ecde00e6972cf17294b593840
Florian Gilcher <florian.gilcher@asquera.de>
2015-10-20 20:13:49 +0200

    Deactivate deploy plugib

--- config.rb
+++ config.rb
@@ -59,15 +59,15 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.build_before = true
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
+#activate :deploy do |deploy|
+#  deploy.method = :git
+#  deploy.build_before = true
+#  # Optional Settings
+#  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+#  # deploy.branch   = 'custom-branch' # default: gh-pages
+#  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+#  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+#end
 
 activate :blog do |blog|
   # set options on blog
